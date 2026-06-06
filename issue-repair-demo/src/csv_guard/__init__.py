38e6d0b6e0e015dab6f2d4c9000a1ee97e5810b5
rosie <ryujihyeon6965@gmail.com>
2026-06-06 14:32:06 +0900

    Add csv guard package init

---
+++ issue-repair-demo/src/csv_guard/__init__.py
@@ -0,0 +1,5 @@
+"""CSV utilities for the issue repair demo."""
+
+from .exporter import Person, export_people_summary, parse_people_csv
+
+__all__ = ["Person", "export_people_summary", "parse_people_csv"]
